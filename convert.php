0d68c7ae1ac623c391117ed0e6eef17198923808
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-01-03 11:51:40 +0100

    Style changes + Fully works

--- convert.php
+++ convert.php
@@ -1,34 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $localRate = test_input($_POST["localRate"]);
-
-    $euros = 0.00024 ** $localRate; 
-
-    echo "<h2>Conversion Result:</h2>";
-    echo "<p>Price in Euros: {$euros}</p>";
-} else {
-    echo "Invalid request";
-}
-
-function test_input(&localRate) {
-    return $euros;
-}
-?>
-
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    echo "Invalid request";
-} else {
-    $localRate = test_input($_POST["localRate"]);
-
     $euros = 0.00024 * $localRate; 
 
     echo "<h2>Conversion Result:</h2>";
-    echo "<p>Price in Euros: {$euros}</p>";
+    echo "<p>Price in Euros: € {$euros}</p>";
+} else {
+    echo "Invalid request";
 }
 
-function test_input(&$localRate) {
-    $localRate = htmlspecialchars($localRate); // Sanitize the input
-    return $localRate;
+function test_input($input) {
+    $input = htmlspecialchars($input); // Sanitize the input
+    return $input;
 }
+?>
