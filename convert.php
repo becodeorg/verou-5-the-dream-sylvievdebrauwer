ee813e6c88024014eaf7130fa0c52ca1de65606d
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-01-03 10:20:24 +0100

    php Changes

--- convert.php
+++ convert.php
@@ -1,10 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve the local currency rate from the form data
+
     $localRate = floatval($_POST["localRate"]);
 
-    $euros = 0,00024 / $localRate; 
+    $euros = 0.00024 / $localRate; 
 
+    echo "<h2>Conversion Result:</h2>";
     echo "<p>Price in Euros: {$euros}</p>";
 } else {
     echo "Invalid request";
