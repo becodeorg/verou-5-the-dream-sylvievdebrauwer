d3ed55be74e1a5eb26d6b18f9f98b14f5884ad9b
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-01-03 14:35:59 +0100

    Added dollars

--- convert.php
+++ convert.php
@@ -1,10 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $localRate = test_input($_POST["localRate"]);
-    $euros = 0.00024 * $localRate; 
+    $currencyType = $_POST["currencyType"];
 
-    echo "<h2>Conversion Result:</h2>";
-    echo "<p>Price in Euros: € {$euros}</p>";
+    $conversionRates = [
+        'euro' => 0.00024,
+        'dollar' => 0.018,
+    ];
+
+    if (isset($conversionRates[$currencyType])) {
+        $conversionRate = $conversionRates[$currencyType];
+        $convertedAmount = $localRate * $conversionRate;
+
+        echo "<h2 class='conversion-result'>Conversion Result:</h2>";
+        echo "<p class='converted-amount'>Price in {$currencyType}: {$convertedAmount} {$currencyType}</p>";
+    } else {
+        echo "Invalid currency type";
+    }
 } else {
     echo "Invalid request";
 }
